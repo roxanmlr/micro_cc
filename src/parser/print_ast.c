d3c43212a225999541761482f1575daa99152eed
roxanmlr <roxanmlr@gmail.com>
2026-07-21 13:23:06 +0200

    modified:   src/parser/print_ast.c 	modified:   tests/nlsandler/chapter_1/valid/spaces.c

--- src/parser/print_ast.c
+++ src/parser/print_ast.c
@@ -1,58 +1,51 @@
 #include "parser.h"
 #include <stdio.h>
+#include <unistd.h>
 
-void print_exp_node(struct exp_node *exp_node, int level){
-	for (int i = 0; i < level; i++)
-		fputc('\t', stderr);
+static inline void putnchar(char c, int n){
+	char p[n+1];
+	memset(p, c, n);
+	p[n] = 0;
+	write(STDERR_FILENO,p, n);
+}
+
+void print_exp_node(struct exp_node *exp_node){
 	if (!exp_node){
 		fprintf(stderr, "NULL EXPR NODE");
 		return;
 	}
-	fprintf(stderr, "(CONST %d\n", exp_node->constant);
-	for (int i = 0; i < level; i++)
-		fputc('\t', stderr);
-	fprintf(stderr, ")\n");
+	fprintf(stderr, "(CONST %d)", exp_node->constant);
 }
 
-void print_ret_node(struct ret_node *ret_node, int level){
-	for (int i = 0; i < level; i++)
-		fputc('\t', stderr);
+void print_ret_node(struct ret_node *ret_node){
 	if (!ret_node){
 		fprintf(stderr, "NULL RETURN NODE");
 		return;
 	}
-	fprintf(stderr, "(RETURN \n");
-	print_exp_node(ret_node->node, level+1);
-	for (int i = 0; i < level; i++)
-		fputc('\t', stderr);
-	fprintf(stderr, ")\n");
+	fprintf(stderr, "(RETURN ");
+	print_exp_node(ret_node->node);
+	fprintf(stderr, ")");
 }
 
 void print_stmt_node(struct stmt_node *stmt_node, int level){
-	for (int i = 0; i < level; i++)
-		fputc('\t', stderr);
-	if (!stmt_node){
-		fprintf(stderr, "NULL STATEMENT NODE");
-		return;
-	}
-	fprintf(stderr, "(STMT \n");
-	print_ret_node(stmt_node->node, level+1);
-	for (int i = 0; i < level; i++)
-		fputc('\t', stderr);
-	fprintf(stderr, ")\n");
+	(void)level;
+	print_ret_node(stmt_node->node);
+	fprintf(stderr,")\n");
 }
 
 void print_func_node(struct func_node *func_node, int level){
-	for (int i = 0; i < level; i++)
-		fputc('\t', stderr);
+	putnchar('\t', level);
 	if (!func_node){
 		fprintf(stderr, "NULL FUNCTION NODE");
 		return;
 	}
-	fprintf(stderr, "(Function %s\n", func_node->name);
+	fprintf(stderr, "(Function\n");
+	putnchar('\t', level + 1);
+	fprintf(stderr, "name=\"%s\",\n", func_node->name);
+	putnchar('\t', level + 1);
+	fprintf(stderr, "body=");
 	print_stmt_node(func_node->node, level+1);
-	for (int i = 0; i < level; i++)
-		fputc('\t', stderr);
+	putnchar('\t', level);
 	fprintf(stderr, ")\n");
 }
 void print_ast(struct program_node *ast){
--- tests/nlsandler/chapter_1/valid/spaces.c
+++ tests/nlsandler/chapter_1/valid/spaces.c
@@ -1,1 +1,2 @@
    int   main    (  void)  {   return  0 ; }
+   int   and    (  void)  {   return  0 ; }
